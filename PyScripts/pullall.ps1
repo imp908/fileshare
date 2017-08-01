577e2e4503b82a4dfee54c7f0a7f5fbc2d202ca2
imp908 <imp908@users.noreply.github.com>
2017-08-01 20:45:12 +0300

    pull all

---
+++ PyScripts/pullall.ps1
@@ -0,0 +1,10 @@
+G:
+cd G:\disk\Files\git\Fileshare
+git init
+git pull https://github.com/imp908/Fileshare.git br1
+cd G:\disk\Files\git\SB
+git init
+git pull https://github.com/imp908/SB.git br1
+cd G:\disk\Files\git\Repo
+git init
+git pull https://github.com/imp908/Repo.git change
