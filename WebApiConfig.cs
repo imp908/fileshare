23966f316ddee91c141092a8702d8d3e41532a18
imp908 <imp908@users.noreply.github.com>
2017-12-11 23:06:33 +0300

    CT,wbpcf

---
+++ WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableCors();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+              name: "DefaultApiNoCtrl",
+              routeTemplate: "api/{controller}/{action}",
+              defaults: new { action = RouteParameter.Optional }
+          );
+
+        }
+    }
+}
