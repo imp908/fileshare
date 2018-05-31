4269d7367938f62399abcff4d6c534e63a92cf0f
imp908 <imp908@users.noreply.github.com>
2018-05-31 15:13:28 +0300

    napi

---
+++ napi/mng/Controllers/Person2Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using NSQLManager;
+
+using System.Configuration;
+
+namespace NewsAPI.Controllers
+{
+
+
+    public class Person2Controller : ApiController
+    {
+        public NewsUOWs.NewsRealUow _newsUOW;
+        WebManagers.ReturnEntities _response;
+         Managers.Manager mng;
+
+        public Person2Controller()
+        {
+          string host_= string.Format("{0}:{1}"
+        ,ConfigurationManager.AppSettings["OrientProdHost"],ConfigurationManager.AppSettings["OrientPort"]);
+
+        mng = new Managers.Manager(
+        ConfigurationManager.AppSettings["OrientProdDB"]
+        ,host_
+        ,ConfigurationManager.AppSettings["orient_login"]
+        ,ConfigurationManager.AppSettings["orient_pswd"]
+        );
+
+          _newsUOW = mng.GetNewsUOW();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Acc()
+        {
+          string result_=mng.UserAcc();
+          _response = new WebManagers.ReturnEntities(result_, Request);
+          return _response;
+        }
+
+    }
+}
---
+++ napi/mng/Controllers/PersonBirthdaysController.cs
@@ -0,0 +1,38 @@
+﻿using NewsAPI.Helpers;
+using NewsAPI.Interfaces;
+using System.Web.Http;
+
+namespace NewsAPI.Controllers
+{
+    public class PersonBirthdaysController : ApiController
+    {
+        private readonly IAddressBookProxy proxy;  
+        private readonly IUserSettings userSettings;
+        private readonly IPersonBirhtdays personBirthdays;
+
+        public PersonBirthdaysController(IUserSettings userSettings,  IPersonBirhtdays personBirthdays, IAddressBookProxy proxy)
+        {
+            this.proxy = proxy;
+            this.personBirthdays = personBirthdays;
+            this.userSettings = userSettings;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            // Получаем хелпер
+            var newsHelper = new OrientNewsHelper();
+
+            // Осуществляем авторизацию в OrientDb
+            newsHelper.Authorize();
+
+            var birthdays = personBirthdays.GetActualPersonBirthdays();
+
+
+            var bdays = new OrientNewsHelper.ReturnPersonsBirthdays(birthdays);
+
+            return bdays;
+                
+        }
+    }
+}
